49b8409ecbcd7c8d0181f59800e56c69d626bacf
Igor Marques da Silva <igormarquessilva@gmail.com>
2015-04-16 13:56:27 -0300

    Initial version of choose org page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
   end
+  def select_org
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#styleguide'
+
+  get 'styleguide/select_org' => 'home#select_org'
+
 end
