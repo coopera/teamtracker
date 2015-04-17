aeeb29e253e0b275849224ce62510edb5d564313
Lucas Bibiano <lucasbibianodossantos@gmail.com>
2015-04-17 15:06:39 -0300

    Read data from slack using rtm-api

--- app/models/slack_notification.rb
+++ app/models/slack_notification.rb
@@ -2,6 +2,7 @@ class SlackNotification
   include Mongoid::Document
   
   field :text, type: String
+  field :channel_id, type: String
   field :user_id, type: String
   field :timestamp, type: DateTime
 end
