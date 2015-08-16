0f65ea5957fe606ecc78bf3f97d4f252a4d4e917
Bernardo Gurgel Filho <bernardogfilho@gmail.com>
2015-08-16 18:40:22 -0300

    Improve data in pull request factory

--- app/factories/pull_request_stub_factory.rb
+++ app/factories/pull_request_stub_factory.rb
@@ -4,7 +4,7 @@ class PullRequestStubFactory
     pull_requests = Array.new
     actions = [:opened, :commented, :closed]
     n.times do
-      pull_requests << PullRequestStub.new(title: 'Some title', time: Time.now, action: actions.sample, author: users.sample, repo: 'coopera/teamtracker', repo_url: "#", url: "#")
+      pull_requests << PullRequestStub.new(title: 'Some title', time: rand(1.week.ago..Time.now), action: actions.sample, author: users.sample, repo: 'coopera/teamtracker', repo_url: "#", url: "#")
     end
     pull_requests
   end
