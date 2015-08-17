0440d47351c99f0891779689e7a92805efebdc42
Bernardo Gurgel Filho <bernardogfilho@gmail.com>
2015-08-17 00:06:22 -0300

    Move pull request composed data from view to presenter

--- app/presenters/pull_request_presenter.rb
+++ app/presenters/pull_request_presenter.rb
@@ -7,4 +7,8 @@ class PullRequestPresenter < ApplicationPresenter
     super(pull_request)
   end
 
+  def info
+    %(<a href="#{self.author.url}">#{self.author.username}</a> #{self.action} <a href="#{self.url}">#{self.title}</a> in <a href="#{self.repo_url}">#{self.repo}</a>).html_safe
+  end
+
 end
