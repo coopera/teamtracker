c0a79bf0fdfb63e76946b3b3e863f5a00c14bce1
Bernardo Gurgel Filho <bernardogfilho@gmail.com>
2015-08-23 22:49:39 -0300

    Remove duplicate requires at UserStubFactory

--- app/factories/user_stub_factory.rb
+++ app/factories/user_stub_factory.rb
@@ -6,8 +6,6 @@ class UserStubFactory
   def self.fabric(n)
     users = Array.new
     n.times do
-      require 'open-uri'
-      require 'json'
       user = JSON.load(open('https://randomuser.me/api/')).first.last.first["user"]
       user_stub = UserStub.new(username: user['username'], url: "#", avatar_url: user['picture']['thumbnail'])
       users << UserPresenter.new(user_stub)
