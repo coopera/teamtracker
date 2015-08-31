f4b909142f20aaba54c57c2356f3984a478e7aa7
Bernardo Gurgel Filho <bernardogfilho@gmail.com>
2015-08-31 19:06:14 -0300

    Get more data from github user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,16 @@ class User
   field :slack_team_id, type: String
   field :provider, type: String
   field :uid, type: Integer
-  field :name, type: String 
+  field :name, type: String
+  field :email, type: String
+  field :avatar, type: String
 
   def self.omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
+      user.email = auth['info']['email']
+      user.avatar = auth['info']['image']
       user.name = auth['info']['name'] || "" if auth['info']
     end
   end
