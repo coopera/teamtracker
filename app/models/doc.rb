17f46ccd5a6065bd734b9d23b9dcf53d54fba842
Igor Marques <igormarquessilva@gmail.com>
2015-09-06 17:45:22 -0600

    Add missing fields to related link

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,11 +1,14 @@
 class Doc
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :title, type: String
   field :description, type: String
   field :implementation, type: String
 
   has_many :related_links
+  # embeds_many :related_links
+
   accepts_nested_attributes_for :related_links, reject_if: :all_blank, allow_destroy: true
 
   validates_presence_of :title, :description, :implementation
--- app/models/related_link.rb
+++ app/models/related_link.rb
@@ -1,10 +1,14 @@
 class RelatedLink
 
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   # field :doc_id, type: String
+  field :title, type: String
+  field :link, type: String
 
   belongs_to :doc
+  # embedded_in :doc
 
   validates_presence_of :link, :title
 end
