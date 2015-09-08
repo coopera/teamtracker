e3bc5dd043891cece66dbf7c9cb6eec3faa04495
Igor Marques <igormarquessilva@gmail.com>
2015-09-07 21:22:03 -0600

    Remove some comments #42

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -8,8 +8,7 @@ class Doc
   field :implementation, type: String
 
   has_many :related_links
-  # embeds_many :related_links
-
+  
   accepts_nested_attributes_for :related_links, reject_if: :all_blank, allow_destroy: true
 
   validates_presence_of :title, :description, :implementation
@@ -19,6 +18,4 @@ class Doc
   def tag_list
     tags.split(",")
   end
-  # acts_as_ordered_taggable
-  # I need to find a mongo solution
 end
