554013037240f1ba855aacd5c395a4d2efd0eec7
Igor Marques <igormarquessilva@gmail.com>
2015-09-21 11:11:40 -0300

    Oauth now gets user access token at signup #47

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,14 +10,19 @@ class User
   field :name, type: String
   field :email, type: String
   field :avatar, type: String
+  field :token, type: String
+  field :nickname, type: String
 
   def self.omniauth(auth)
     create! do |user|
+      # raise Exception.new auth
       user.provider = auth['provider']
       user.uid = auth['uid']
       user.email = auth['info']['email']
       user.avatar = auth['info']['image']
       user.name = auth['info']['name'] || "" if auth['info']
+      user.token = auth['credentials']['token']
+      user.nickname = auth['info']['nickname']
     end
   end
 end
